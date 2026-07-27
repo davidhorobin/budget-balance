d80a41f8f61d4ec65ee979f11c696394d2330e28
David Horobin <dwghorobin@gmail.com>
2026-07-27 23:16:33 +0100

    init

---
+++ src/main/java/com/davidhorobin/budgetbalance/BudgetBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.davidhorobin.budgetbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidhorobin/budgetbalance/BudgetBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidhorobin.budgetbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
