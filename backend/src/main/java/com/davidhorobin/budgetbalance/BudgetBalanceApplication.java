18cad1c11f4c5adbefb2c7429a8f21a9c57b3581
David Horobin <dwghorobin@gmail.com>
2026-07-28 15:25:03 +0100

    postgresql and basic rest api

--- backend/src/main/java/com/davidhorobin/budgetbalance/BudgetBalanceApplication.java
+++ backend/src/main/java/com/davidhorobin/budgetbalance/BudgetBalanceApplication.java
@@ -1,11 +1,7 @@
 package com.davidhorobin.budgetbalance;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.NoSuchElementException;
 
 @SpringBootApplication
 public class BudgetBalanceApplication {
