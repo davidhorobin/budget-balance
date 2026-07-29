2f0da5b523bc47a3ed17413506c8dac073c4ed69
David Horobin <dwghorobin@gmail.com>
2026-07-29 23:12:55 +0100

    add: security filter chain scaffold

--- backend/src/main/java/com/davidhorobin/budgetbalance/config/SecurityConfig.java
+++ backend/src/main/java/com/davidhorobin/budgetbalance/config/SecurityConfig.java
@@ -2,11 +2,35 @@ package com.davidhorobin.budgetbalance.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/transaction/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // TODO Create login controller
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
