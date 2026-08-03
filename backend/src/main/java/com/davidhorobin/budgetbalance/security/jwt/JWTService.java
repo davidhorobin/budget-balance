5f61d9e715ef4559cec089af8de5c2c05d95bb0a
David Horobin <dwghorobin@gmail.com>
2026-08-03 17:41:06 +0100

    fix: set access token expiry to 15 minutes

--- backend/src/main/java/com/davidhorobin/budgetbalance/security/jwt/JWTService.java
+++ backend/src/main/java/com/davidhorobin/budgetbalance/security/jwt/JWTService.java
@@ -38,7 +38,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
                 .and()
                 .signWith(getKey())
                 .compact();
