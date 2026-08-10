5f9b3188539c1585c4730669245da1964aaad849
David Horobin <dwghorobin@gmail.com>
2026-08-10 16:02:00 +0100

    add: useAuth hook

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,11 +1,11 @@
-import {useRef, useState, useEffect, useContext} from 'react'
-import AuthContext from './context/AuthProvider'
+import {useState, useEffect} from 'react'
+import useAuth from "./hooks/useAuth";
 import axios from './api/axios'
 
 const LOGIN_URL = '/auth/login'
 
 const Login = () => {
-    const {setAuth} = useContext(AuthContext);
+    const {setAuth} = useAuth();
 
     const [user, setUser] = useState('');
     const [pwd, setPwd] = useState('');
