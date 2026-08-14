581d9775f7f513135f42ed0b6092cfdb9a7e3eaa
David Horobin <dwghorobin@gmail.com>
2026-08-14 22:24:00 +0100

    add: email field

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,11 +4,15 @@ import axios from '../api/axios'
 
 const USER_REGEX = /^[A-Za-z0-9]{2,24}$/
 const PASS_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,24}$/
+const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
 const REGISTER_URL = '/auth/register'
 
 const Register = () => {
-    const userRef = useRef();
     const errRef = useRef();
+
+    const [email, setEmail] = useState("");
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
 
     const [user, setUser] = useState('');
     const [validName, setValidName] = useState(false);
@@ -26,36 +30,38 @@ const Register = () => {
     const [success, setSuccess] = useState(false);
 
     useEffect(() => {
-        userRef.current.focus();
-    }, [])
+        const result = EMAIL_REGEX.test(email);
+        setValidEmail(result);
+    }, [email]);
 
     useEffect(() => {
         const result = USER_REGEX.test(user);
         setValidName(result);
-    }, [user])
+    }, [user]);
 
     useEffect(() => {
         const result = PASS_REGEX.test(pwd);
         setValidPwd(result);
         const match = pwd === matchPwd;
         setValidMatchPwd(match);
-    }, [pwd, matchPwd])
+    }, [pwd, matchPwd]);
 
     useEffect(() => {
         setErrMsg('');
-    }, [user, pwd, matchPwd])
+    }, [email, user, pwd, matchPwd]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const v1 = USER_REGEX.test(user);
-        const v2 = PASS_REGEX.test(pwd);
-        if (!v1 || !v2) {
+        const v1 = EMAIL_REGEX.test(email)
+        const v2 = USER_REGEX.test(user);
+        const v3 = PASS_REGEX.test(pwd);
+        if (!v1 || !v2 || !v3) {
             setErrMsg('Invalid details');
             return;
         }
         try {
             await axios.post(REGISTER_URL,
-                JSON.stringify({username: user, password: pwd, email: "brian@email.com"})
+                JSON.stringify({username: user, password: pwd, email: email})
             );
             setSuccess(true);
         } catch (err) {
@@ -81,12 +87,27 @@ const Register = () => {
                     <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
                     <h1>Register</h1>
                     <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            autoComplete="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            aria-invalid={validEmail ? "false" : "true"}
+                            aria-describedby="emailnote"
+                            onFocus={() => setEmailFocus(true)}
+                            onBlur={() => setEmailFocus(false)}
+                        />
+                        <p id="emailnote"
+                           className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
+                            Please enter a valid email address
+                        </p>
                         <label htmlFor="username">Username</label>
                         <input
                             type="text"
                             id="username"
-                            ref={userRef}
-                            autoComplete="off"
+                            autoComplete="username"
                             onChange={(e) => setUser(e.target.value)}
                             required
                             aria-invalid={validName ? "false" : "true"}
@@ -102,6 +123,7 @@ const Register = () => {
                         <input
                             type="password"
                             id="password"
+                            autoComplete="new-password"
                             onChange={(e) => setPwd(e.target.value)}
                             required
                             aria-invalid={validPwd ? "false" : "true"}
@@ -109,7 +131,7 @@ const Register = () => {
                             onFocus={() => setPwdFocus(true)}
                             onBlur={() => setPwdFocus(false)}
                         />
-                        <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                        <p id="pwdnote" className={pwdFocus && email && !validPwd ? "instructions" : "offscreen"}>
                             Must be between 8 and 24 characters <br/>
                             Must contain at least one uppercase letter, lowercase letter, number, and special character.
                         </p>
@@ -124,7 +146,8 @@ const Register = () => {
                             onFocus={() => setMatchFocus(true)}
                             onBlur={() => setMatchFocus(false)}
                         />
-                        <p id="confpwdnote" className={matchFocus && !validMatchPwd ? "instructions" : "offscreen"}>
+                        <p id="confpwdnote"
+                           className={matchFocus && matchPwd && !validMatchPwd ? "instructions" : "offscreen"}>
                             Must match password
                         </p>
                         <button disabled={!validName || !validPwd || !validMatchPwd}>Register</button>
