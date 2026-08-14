c70f9404a086a97ca88eac31ca072c74f018ceed
David Horobin <dwghorobin@gmail.com>
2026-08-14 16:00:09 +0100

    add: AccountsMapper toEntity test

---
+++ backend/src/test/java/com/davidhorobin/budgetbalance/mapper/AccountsMapperTest.java
@@ -0,0 +1,30 @@
+package com.davidhorobin.budgetbalance.mapper;
+
+import com.davidhorobin.budgetbalance.dto.accounts.CreateRequest;
+import com.davidhorobin.budgetbalance.entity.BankAccount;
+import com.davidhorobin.budgetbalance.entity.Counterparty;
+import com.davidhorobin.budgetbalance.entity.User;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class AccountsMapperTest {
+    @Test
+    void toEntity_mapsAllFields() {
+        CreateRequest request = new CreateRequest("Current", "HSBC", BigDecimal.valueOf(150), "GBP");
+        String name = "current";
+        User user = new User();
+        Counterparty counterparty = new Counterparty();
+
+        BankAccount a = AccountsMapper.toEntity(request, name, user, counterparty);
+
+        assertEquals("current", a.getName());
+        assertSame(user, a.getUser());
+        assertSame(counterparty, a.getCounterparty());
+        assertEquals(BigDecimal.valueOf(150), a.getBalance());
+        assertEquals("GBP", a.getCurrency());
+    }
+}
